2fb2b20db850a3523f72e6140e2c158dd5282eaa
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-09-08 21:07:31 +0300

    Main luokan kommentti muutettu

--- KalastusApu/src/main/java/kalastusapuharkka/kalastusapu/Main.java
+++ KalastusApu/src/main/java/kalastusapuharkka/kalastusapu/Main.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Tämä Main luokka on luotu vain testiä varten
  */
 package kalastusapuharkka.kalastusapu;
 
@@ -10,5 +8,5 @@ package kalastusapuharkka.kalastusapu;
  * @author mikko
  */
 public class Main {
-    
+
 }
