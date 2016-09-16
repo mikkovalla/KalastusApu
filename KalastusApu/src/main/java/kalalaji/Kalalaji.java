76e10ca6da72c4d7b9755345e3c72357f57138b6
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-09-16 22:45:00 +0300

    kalalaji pakkauksen Kalalaji rajapinta

---
+++ KalastusApu/src/main/java/kalalaji/Kalalaji.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalalaji;
+
+/**
+ *
+ * @author mikko
+ */
+public interface Kalalaji {
+
+    public void setLaji(String laji);
+
+    public String getLaji();
+
+    public void setEsiintyminen(String es);
+
+    public String getEsiintyminen();
+}
