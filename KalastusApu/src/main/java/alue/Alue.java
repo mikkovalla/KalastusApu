941a7817765830f94eba737f6cf8cdb24c4b290d
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-09-16 22:46:22 +0300

    alue pakkauksen Alue rajapinta

---
+++ KalastusApu/src/main/java/alue/Alue.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alue;
+
+/**
+ *
+ * @author mikko
+ */
+public interface Alue {
+
+    public String getAlue();
+}
