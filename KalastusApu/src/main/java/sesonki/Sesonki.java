477bb0c42ffad3882b590ecb07e19726c3df86f2
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-09-16 22:48:01 +0300

    sesonki pakkauksen Sesonki rajapinta

---
+++ KalastusApu/src/main/java/sesonki/Sesonki.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesonki;
+
+import java.util.Date;
+
+/**
+ *
+ * @author mikko
+ */
+public interface Sesonki {
+
+    public String getSesonki();
+
+}
