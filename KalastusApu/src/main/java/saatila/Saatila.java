87709a39d2a6ebff4955595f85f35c02f8fd5548
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-09-16 22:47:16 +0300

    saatila pakkauksen Saatila rajapinta
    Tähän lisätään openweathermapin paikallis sää data APIn kautta

---
+++ KalastusApu/src/main/java/saatila/Saatila.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saatila;
+
+/**
+ *
+ * @author mikko
+ * Säätilan tuottamiseen käytän openweathermap:in tarjoamaa apia jonka alku oletuksena on Helsinki, tarkoitus
+ * kuitenkin on antaa käyttäjän valita sääpaikannus itse.
+ */
+public interface Saatila {
+    
+}
