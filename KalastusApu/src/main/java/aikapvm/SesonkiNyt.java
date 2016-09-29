2e555f268302d0a3fdaa994c8f5202226fbe2ddc
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-09-29 03:42:41 +0300

    SesonkiNyt luokka siirretty aikapvm pakkaukseen

---
+++ KalastusApu/src/main/java/aikapvm/SesonkiNyt.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aikapvm;
+
+/**
+ * @author mikko
+ */
+public class SesonkiNyt {
+
+    private Kuukaudet kN;
+    private Kuukausi kk;
+
+    public SesonkiNyt() {
+        kk = new Kuukausi();
+    }
+
+    public void setSesonki(String sesonki) {
+        kN.setKuukaudenSesonki(sesonki);
+    }
+
+    public String getSesonki() {
+        return kk.getKuuNimi().getKuukaudenSesonki();
+    }
+
+    @Override
+    public String toString() {
+        if (!getSesonki().equals("TALVI") || !getSesonki().equals("KEVÄT")) {
+            return kk.toString() + ", eli nyt on " + getSesonki() + "n sesonki.";
+        }
+        return kk.toString() + ", eli nyt on " + getSesonki() + " sesonki.";
+    }
+}
