59833d55db3b9498829a479684ffd5da28cc46a5
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-09-30 06:03:29 +0300

    tulossa

--- KalastusApu/src/main/java/saatila/logiikka/SaaNyt.java
+++ KalastusApu/src/main/java/saatila/logiikka/SaaNyt.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package saatila;
+package saatila.logiikka;
 
 /**
  *
  * @author mikko
- * Säätilan tuottamiseen käytän openweathermap:in tarjoamaa apia jonka alku oletuksena on Helsinki, tarkoitus
- * kuitenkin on antaa käyttäjän valita sääpaikannus itse.
  */
-public interface Saatila {
+public class SaaNyt {
     
 }
