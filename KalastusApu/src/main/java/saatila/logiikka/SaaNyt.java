5c36cb52eb787f1946f7a986fcbe7abf5c167514
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-10-04 12:27:10 +0300

    luotu

--- KalastusApu/src/main/java/saatila/logiikka/SaaNyt.java
+++ KalastusApu/src/main/java/saatila/logiikka/SaaNyt.java
@@ -5,10 +5,36 @@
  */
 package saatila.logiikka;
 
+import saatila.saa.Pilvisyys;
+import saatila.saa.Sade;
+import saatila.saa.Tuuli;
+
 /**
+ * Luokan SaaNyt tarkoitus on kerätä yhteen luokan ilmentymään säätilaa koskevat
+ * tiedot.
  *
  * @author mikko
  */
 public class SaaNyt {
-    
+
+    private Pilvisyys pilvet;
+    private Sade sade;
+    private Tuuli tuuli;
+
+    public SaaNyt(Pilvisyys pilvet, Sade sade, Tuuli tuuli) {
+        this.pilvet = pilvet;
+        this.sade = sade;
+        this.tuuli = tuuli;
+    }
+
+    public void setSaaNyt(Pilvisyys p, Sade s, Tuuli t) {
+        this.pilvet = p;
+        this.sade = s;
+        this.tuuli = t;
+    }
+
+    @Override
+    public String toString() {
+        return "Sää: \nPilvisyys: " + this.pilvet.toString() + "\nSade: " + this.sade.toString() + "\n" + this.tuuli.toString();
+    }
 }
