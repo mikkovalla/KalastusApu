c3b49237cc1973275a33602f11ed8024158f51f0
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-10-06 02:01:08 +0300

    päivitetty

--- KalastusApu/src/main/java/logiikka/Main.java
+++ KalastusApu/src/main/java/logiikka/Main.java
@@ -3,12 +3,10 @@ Tämä Main luokka on luotu vain testiä varten
  */
 package logiikka;
 
-import aikapvm.Kuukaudet;
-import aikapvm.Kuukausi;
 import alue.AlueValinta;
 import aikapvm.SesonkiNyt;
-import static alue.Vesi.JÄRVI;
 import static alue.Vesi.MERI;
+import kalalaji.kalat.Kalat;
 import saatila.logiikka.SaaNyt;
 import saatila.saa.Pilvisyys;
 import saatila.saa.Sade;
@@ -27,13 +25,14 @@ public class Main {
     public static void main(String[] args) {
 
         SesonkiNyt sn = new SesonkiNyt();
-        Kuukausi kk = new Kuukausi();
         AlueValinta av = new AlueValinta(MERI, "", 15, 10);
         Tuuli t = new Tuuli(ETELÄ, 5);
         Pilvisyys p = new Pilvisyys(AURINKOINEN);
         Sade s = new Sade(POUTA);
         SaaNyt saa = new SaaNyt(p, s, t);
 
-        System.out.println(sn.toString() + "\n" + av.toString() + "\n" + saa.toString());
+        System.out.println(sn.toString() + "\n" + av.toString() + "\n" + saa.toString() + "\n");
+        System.out.println("Kohde kala: " + Kalat.Hauki + "\n");
+        System.out.println("Esiintyminen: " + Kalat.Hauki.esiintyySesonginMukaan());
     }
 }
