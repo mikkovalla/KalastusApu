82e17902e0dde5f18087c10f0de03c1925da5864
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-10-07 19:05:52 +0300

    javadoc korjattu

--- KalastusApu/src/main/java/kalalaji/kalat/Kalat.java
+++ KalastusApu/src/main/java/kalalaji/kalat/Kalat.java
@@ -10,23 +10,47 @@ import kalalaji.esiintyminen.*;
 import kalalaji.kalastustapa.*;
 
 /**
+ * Vakio luokka joka luo yhden olion joka sisältää esiintymisen sekä
+ * kalastustavan sesongin mukaan.
  *
- * @author mikko
  */
 public enum Kalat {
     //Lisättynä vain hauki jotta ohjelma pysyisi siedettävässä mittakaavassa tässä vaiheessa.
     //Ahven, Kuha, Taimen;
-    Hauki(HaukiEsiintyminen.HAUKI);
+    Hauki(HaukiEsiintyminen.HAUKI, HaukiKalastus.HAUKI);
 
     private Esiintyminen es;
     private Kalastustapa kt;
     private SesonkiNyt sn = new SesonkiNyt();
 
-    Kalat(Esiintyminen es) {
+    /**
+     * Konstrukstori asettaa vakio arvolle sitä vastaavat parametrit.
+     *
+     * @param es on Esiintyminen rajapinta joka sisältää kaikki rajapinnan
+     * toteuttavat luokat
+     * @param kt on Kalastustapa rajapinta joka sisältää kaikki rajapinnan
+     * toteuttavat luokat
+     */
+    Kalat(Esiintyminen es, Kalastustapa kt) {
         this.es = es;
+        this.kt = kt;
     }
 
+    /**
+     * Metodi palauttaa vakio arvona annetun kalan esiintymisen sesongin mukaan.
+     *
+     * @return esiintyminen sesongin mukaan String tyyppisenä.
+     */
     public String esiintyySesonginMukaan() {
         return es.esiintyySesonginMukaan(es, sn);
+    }
+
+    /**
+     * Metodi hakee vakio kalaa koskevan kalastustavan.
+     *
+     * @return kalastus tapa sesongin mukaan String tyyppisenä.
+     */
+    public String kalastustapaSesonginMukaan() {
+        return kt.kalastusSesonginMukaan(kt, sn);
     }
 }
