369ed2b96eaaafd00360c28df47ef496967ff4fc
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-10-07 19:05:44 +0300

    luokka luotu

--- KalastusApu/src/main/java/kalalaji/kalastustapa/Kalastustapa.java
+++ KalastusApu/src/main/java/kalalaji/kalastustapa/Kalastustapa.java
@@ -5,10 +5,54 @@
  */
 package kalalaji.kalastustapa;
 
+import aikapvm.SesonkiNyt;
+
 /**
+ * Kalastustapa rajapinta jossa määritetään pakkauksessa kalalaji.kalastustapa
+ * olevien luokkien toteuttamat metodit. Rajapintaa käytetään myös selventämään
+ * pakkauksen luokkien olioiden toimintaa sekä perinnöllisyyttä, ja helpottamaan
+ * uusien lajien lissämistä ohjelmaan.
  *
- * @author mikko
  */
 public interface Kalastustapa {
 
+    /**
+     * Rajapinnan metodi joka palauttaa kalalajin talvi kalastus metodit.
+     *
+     * @return talvi kalastustapa String tyyppisenä.
+     */
+    public String getTalviKalastus();
+
+    /**
+     * Rajapinnan metodi joka palauttaa kalalajin kevät kalastus metodit.
+     *
+     * @return kevät kalastustapa String tyyppisenä.
+     */
+    public String getKevatKalastus();
+
+    /**
+     * Rajapinnan metodi joka palauttaa kalalajin kesä kalastus metodit.
+     *
+     * @return kesä kalastustapa String tyyppisenä.
+     */
+    public String getKesaKalastus();
+
+    /**
+     * Rajapinnan metodi joka palauttaa kalalajin syksy kalastus metodit.
+     *
+     * @return syksy kalastustapa String tyyppisenä.
+     */
+    public String getSyksyKalastus();
+
+    /**
+     * Metodi palauttaa tiedon string tyyppisenä missä haluttu laji esiintyy
+     * sesongin mukaan.
+     *
+     * @param kt on Kalastustapa rajapinnan ilmentymä joka sisältää kaikki
+     * rajapinnan toteuttavat luokat.
+     * @param sn SesonkiNyt olio joka määrittää ajankohtaisen sesongin.
+     * @return palauttaa kalalajin ennalta sesongin mukaan määritellyn
+     * kalastustavan kuvauksen String tyyppisenä.
+     */
+    public String kalastusSesonginMukaan(Kalastustapa kt, SesonkiNyt sn);
 }
